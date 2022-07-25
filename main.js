933d0eda6de440dd6b94f02c397e7e166f76e1b2
Eriksen Lezama <eriksen.lezama@gmail.com>
2022-07-25 12:38:27 -0500

    Update API url

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 /*https://talaikis.com/api/quotes/random/*/
 
-(function(){
-  
-   // New Quote function
-   function getQuote(){
-     var xmlhttp = new XMLHttpRequest();
-     xmlhttp.onreadystatechange = function() {
-         if (this.readyState == 4 && this.status == 200) {
-             var objQuote = JSON.parse(this.responseText);
-             document.getElementById("text-quote").innerHTML = objQuote.quote;
-             document.getElementById("text-autor").innerHTML = objQuote.author;
-         }
-     };
-     xmlhttp.open("GET", "https://talaikis.com/api/quotes/random/", true);
-     xmlhttp.send();
-   }
-   getQuote();
-   
-   // New Quote button
-   var newQuote = document.getElementById('change');
-   newQuote.addEventListener('click', getQuote);
-   
-   // Twitter 
-   document.getElementById('tweet').addEventListener('click', function(){
-     var text = document.getElementById('text-quote').innerHTML;
-     var author = document.getElementById('text-autor').innerHTML;
-     var textTweet = text + ' - ' + author;
-     var linkTwitter = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(textTweet);
-     window.open(linkTwitter, '_blank');  
-   });
- })();
- 
- 
+(function () {
+
+  // New Quote function
+  async function getQuote() {
+    var req = await fetch('https://api.quotable.io/random')
+    var res = await req.json()
+
+    let content, name = ''
+
+    if (req.status === 200) {
+      content = res.content
+      name = res.author
+    } else {
+      content = res.message + '. Try a few seconds latter.'
+      name = "Error limit"
+    }
+
+    document.getElementById("text-quote").innerHTML = content;
+    document.getElementById("text-autor").innerHTML = name;
+  }
+
+  getQuote();
+
+  // New Quote button
+  var newQuote = document.getElementById('change');
+  newQuote.addEventListener('click', getQuote);
+
+  // Twitter
+  document.getElementById('tweet').addEventListener('click', function () {
+    var text = document.getElementById('text-quote').innerHTML;
+    var author = document.getElementById('text-autor').innerHTML;
+    var textTweet = text + ' - ' + author;
+    var linkTwitter = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(textTweet);
+    window.open(linkTwitter, '_blank');
+  });
+
+})();
